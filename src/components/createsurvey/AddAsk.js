1d714f803aa6ae7e5919670323d6b144a9fd8bcf
Cholong Park <80766477+cholong2@users.noreply.github.com>
2022-10-14 13:53:16 +0900

    개수대로 생성

--- src/components/createsurvey/AddAsk.js
+++ src/components/createsurvey/AddAsk.js
@@ -2,26 +2,61 @@ import React from 'react';
 import { Link} from "react-router-dom";
 import './modal.css';
 import { useSelector, useDispatch } from "react-redux/";
-import {selectOnepage, selectCard} from "../Slice/CreateSurveySlice"
+import { useState} from 'react'
+
+import { increament, increament2, increament3} from "../../components/Slice/OnepageSlice";
+
 
 const AddAsk = (props) => {
+    const [mult, setMult] = useState(1)
+    const [sub, setSub] = useState(1)
+    const [linear, setLinear] = useState(1)
 
     const dispatch = useDispatch();
-    const type = ["단답형", "장문형", "객관식", "체크박스", "드롭다운", "선형배율"];
+
+    const changeMult = (e) =>{
+        setMult(e.target.value)
+        console.log(mult)
+    }
+    const changeSub = (e) =>{
+        setSub(e.target.value)
+    }
+    const changeLine = (e) =>{
+        setLinear(e.target.value)
+    }
+    const add  = () => {
+    for (let i = 0; i <mult-2; i++) {
+        dispatch(increament())
+      }
+    for (let i = 0; i <sub; i++) {
+        dispatch(increament2())
+      }
+    for (let i = 0; i <linear; i++) {
+        dispatch(increament3())
+      }
+    }
+
+
 
   return (
     <div>
-        {type.map((item) =>
-         (
-                <div className='typeCard'>
-                    {item}
-                    <input type = "number" min='0'></input>
-                </div>
-        ))}
-    
+
+        <div className='typeCard'>
+            <div>
+            <input type = "number" min='0' onChange = {changeMult}/>객관식
+            </div>
+            <div>
+            <input type = "number" min='0' onChange = {changeSub}/>주관식
+            </div>
+            <div><input type = "number" min='0' onChange = {changeLine}/>선형배율</div>
+        
+        </div>
+
         <div>
         <Link to="/Onepage">
-        <button>만들기</button>
+        <button onClick={
+            add
+        }>만들기</button>
         </Link>
         </div>
     </div>
