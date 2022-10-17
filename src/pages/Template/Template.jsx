bd0460af8a978e42ec90673269cc56869cf2c900
vbnm134678 <76089508+vbnm134678@users.noreply.github.com>
2022-10-17 13:25:30 +0900

    Template1017(2)

--- src/pages/Template/Template.jsx
+++ src/pages/Template/Template.jsx
@@ -1,14 +1,31 @@
 
 import "./Template.css"
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux/";
+import Modal from "../../components/createsurvey/Modal"
+import SelectType from "../../components/createsurvey/SelectType.js"
+import { exit } from "../../components/Slice/CreateSurveySlice.js"
+import AddAsk from '../../components/createsurvey/AddAsk';
 
 function Template() {
+
+  const dispatch = useDispatch()
+  const next = useSelector((state) => state.createSurvey.next);
+  const [modalOpen, setModalOpen] = useState(false);
 
   const recentList = [["설문조사 이름", "#카테고리"], ["설문조사 이름", "#카테고리"], ["설문조사 이름", "#카테고리"], ["설문조사 이름", "#카테고리"]];
   // db에서 나중에 data 가져오기.
   const popularList = [["설문조사 이름", "#카테고리"], ["설문조사 이름", "#카테고리"], ["설문조사 이름", "#카테고리"], ["설문조사 이름", "#카테고리"]];
 
+  const openModal = () => {
+    setModalOpen(true);
+    console.log(next)
+  };
+  const closeModal = () => {
+    setModalOpen(false);
+    dispatch(exit())
+  };
 
   return (
     <div id="tpwrapper">
@@ -17,11 +34,18 @@ function Template() {
           <h1 class="tptopcontent">
             당신에게 맞는 템플릿을 찾아보세요!
           </h1>
+          <p></p>
           <>UNIQ는 다양한 템플릿을 통해 당신에게 적합한 설문지를 생성하도록 도와줍니다.</>
           <p></p>
-          <Link to="/createSurvey">
-            <button class="tpcreate">설문 생성하러 가기</button>
-          </Link>
+            <button class="tpcreate" onClick={openModal}>설문 생성하러 가기</button>
+            {next ?
+              <Modal open={modalOpen} close={closeModal} header="질문 유형 개수를 선택해주세요.">
+                <AddAsk />
+              </Modal> :
+              <Modal open={modalOpen} close={closeModal} header="설문이 보여지는 유형을 선택해주세요.">
+                <SelectType />
+              </Modal>
+            }
           <p></p>
           <Link to="/login">
             <button class="tplogin">로그인 하기</button>
@@ -54,7 +78,7 @@ function Template() {
           인기있는 UNIQ 템플릿
         </div>
       <div class="tpbuttom">
-        {recentList.map((item) => (
+        {popularList.map((item) => (
           <div id="tpbox">
             <div class="tpboximg" />
             <div class="tpboxdata">
