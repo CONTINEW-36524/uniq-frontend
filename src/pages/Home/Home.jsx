b2b86a1bccb29f223f09b21413452398e2d4dc6e
Cholong Park <80766477+cholong2@users.noreply.github.com>
2022-10-17 23:06:13 +0900

    ui 수정

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -35,10 +35,10 @@ function Home() {
       <div class="list">
         <div class="jumbotron">
           <h1 class="display-8">이제는 설문 템플릿도 공유하자! </h1>
-          <h1 class="display-3">UNIQ</h1>
-          <br />
-          <p>설문조사 무료 디자인 툴</p>
-
+          <br/>  
+          <h1 class="display-3">UNIQ </h1> 
+          <br/>
+          <p>설문조사 무료 디자인 툴<br/>원하는 템플릿을 선택해 간편하게 설문조사를 만들어보세요!</p>
           <div style={{ textDecoration: 'none' }}>
             <button class='btn3' onClick={openModal}> <b> 설문 생성하러 가기 </b></button> <br />
             {next ?
@@ -57,7 +57,7 @@ function Home() {
       </div>
 
       {/* page 2 */}
-      <div class='list'>
+      <div class='list1'>
         <div class='parent'>
           <b class='recent'>최신 UNIQ 템플릿</b>
           <div class="container">
