5323930f9393b5159bbb4b0dc7805161295a03b6
Dajeong Park <jul897813@gmail.com>
2022-11-07 09:29:46 +0900

    Update ImageSlider.jsx
    Update slider properties

--- src/pages/Home/ImageSlider.jsx
+++ src/pages/Home/ImageSlider.jsx
@@ -3,10 +3,10 @@ import styled from 'styled-components';
 import Slider from "react-slick";
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import sampleImg1 from '../../assets/banner4.png';
-import sampleImg2 from '../../assets/banner2.png';
-import sampleImg3 from '../../assets/banner3.png';
-
+import sampleImg1 from '../../assets/banner13.png';
+import sampleImg2 from '../../assets/banner13.png';
+import sampleImg3 from '../../assets/banner13.png';
+import './BannerSlider.css'
 const Container = styled.div`
     overflow:hidden;
 `;
@@ -18,31 +18,35 @@ const StyledSlider = styled(Slider)`
 `;
 
 const ImageContainer = styled.div`
-    margin: 0 400px;
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0;
 `;
 
 const Image = styled.img`
-    width:100%;
-    height:100%;
+    max-width:95%;
+    max-height:500px;
 `;
 
 const imgUrl = require('../../assets/banner3.png');
 
 const items = [
-    { id: 1, url: sampleImg1 },
+    { id: 1, url: sampleImg3 },
     { id: 4, url: sampleImg2 },
     { id: 2, url: sampleImg1 },
     { id: 3, url: sampleImg3 },
-    { id: 5, url: sampleImg1 },
-    { id: 6, url: sampleImg2 },
+    { id: 5, url: sampleImg2 },
+    { id: 6, url: sampleImg1 },
 ];
 
 
 export default class SimpleSlider extends Component {
+    
     render() {
         const settings = {
             infinite: true,
-            speed: 500,
+            speed: 1000,
             slidesToShow: 1,
             slidesToScroll: 1,
             arrows: true,
@@ -60,7 +64,7 @@ export default class SimpleSlider extends Component {
                     return (
                         <div key={item.id}>
                             <ImageContainer>
-                                <Image src={item.url} />
+                                <Image class="sliderImage" src={item.url} />
                             </ImageContainer>
                         </div>
                     );
