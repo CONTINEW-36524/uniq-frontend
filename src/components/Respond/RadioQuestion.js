784e9072973af5d6109368caa35d23ae078e4437
climatc1103 <39791307+climatc1103@users.noreply.github.com>
2022-11-07 12:04:19 +0900

    ㅇㅇ

--- src/components/Respond/RadioQuestion.js
+++ src/components/Respond/RadioQuestion.js
@@ -1,38 +1,49 @@
-import React, { useEffect, useState, useCallback } from 'react';
-import "./Respond.css"
+import React, { useEffect,useState } from 'react';
+import { useSelector, useDispatch } from "react-redux/";
+import {changecontent} from "../../components/Slice/RespondSlice";
+
+function FirstContents(props) {
+    const dispatch = useDispatch();
+  
+    let [btnActive, setBtnActive] = useState();
+  
+    const toggleActive = (e) => {
+      setBtnActive(e)
+      };
 
 
-
-function FirstQuestion() {
-  let data = ['1학년', '2학년', '3학년', '4학년'];
-
-  let [btnActive, setBtnActive] = useState("");
-
-  const btnClick = (e) => {
-    setBtnActive((prev) => {
-      return e.target.value;
-    });
-  };
+    const passcontent = (e) =>{
+        const passs={
+          id: props.data.did,
+          item : e
+        }
+        dispatch(changecontent(passs));
+          
+        
+      };
 
     return (
       <div className="Qlayout">
-        <h3>질문: ____ ?</h3>
+<h3>질문: ____ ?</h3>
 
-        <div className="radioContainer">
-          {data.map((item, idx) => {
-            return (
-              <div>
-                <button
-                  value={idx}
-                  className={"radioBtn" + (idx == btnActive ? "-active" : "")}
-                  onClick={btnClick}
-                >
-                o {item}
-                </button>
-              </div>
-            );
-          })}
-        </div>
+<div className="radioContainer">
+  {data.map((item, idx) => {
+    return (
+      <div>
+        <button
+          value={idx}
+          className={"radioBtn" + (idx == btnActive ? "-active" : "")}
+          onClick={()=>{
+            toggleActive(idx+1);
+            passcontent(item.con);
+            }}
+        >
+        o {item}
+        </button>
+      </div>
+    );
+  })}
+</div>
       </div>
     );
   }
--- src/components/Respond/ShortQuestion.js
+++ src/components/Respond/ShortQuestion.js
@@ -1,18 +1,38 @@
 import React, { useEffect,useState } from 'react';
-import "./Respond.css"
+import { useSelector, useDispatch } from "react-redux/";
+import {changecontent} from "../../components/Slice/RespondSlice";
 
 function SecondQuestion(props) {
+  
+  const data = useSelector((state)=>state.respond.survey.responddata);
+    const dispatch = useDispatch();
+
+
+    const passcontent = (e) =>{
+      const passs={
+        id: props.data.id,
+        item : e
+      }
+      dispatch(changecontent(passs));
+  
+    };
+
+    const answer = data.filter(item => item.id == props.data.id)[0].answer
+    console.log(props.data.id)
+    console.log(data)
   
     
     return (
    
       <div className="Qlayout">
-        <h3>질문: ____ ?</h3>
-        <textarea className="SecondRespond" 
-          type = "text" 
-          placeholder="답변을 입력해주세요."
-        />
-      </div>
+      <h3>질문: ____ ?</h3>
+      <textarea className="SecondRespond" 
+        type = "text" 
+        value={answer}
+        onChange ={(e)=>passcontent(e.target.value)}
+        placeholder="답변을 입력해주세요."
+      />
+    </div>
 
     );
   }
