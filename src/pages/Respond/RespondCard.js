c1d04fc69af9a6f3e5ba7395f5fedf0162ecab19
Dajeong Park <jul897813@gmail.com>
2022-11-07 20:40:46 +0900

    fix: build error
    useParams from 'react-router-dom'

--- src/pages/Respond/RespondCard.js
+++ src/pages/Respond/RespondCard.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState, useParams } from 'react';
-import { useLocation } from 'react-router-dom';
+import React, { useEffect, useState} from 'react';
+import { useLocation, useParams } from 'react-router-dom';
 import "./RespondCard.css"
 import Startreturn from '../../components/Respond/Startreturn';
 import Form from 'react-bootstrap/Form';
